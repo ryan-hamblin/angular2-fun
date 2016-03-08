382e25858e9b1af6226d46305544e886f7db1716
Ryan Hamblin <crash_bandicoot@Ryans-MacBook-Pro.local>
2016-03-08 15:46:48 -0700

    first build

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,7 @@
 import {Component} from 'angular2/core';
 
 @Component({
-		selector: 'my-app',
-		template: '<h1>This is my first A2 App!!</h1>'
-
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
 })
-
-export class AppComponent { } 
+export class AppComponent { }
